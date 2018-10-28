3641df612d5d78091603ffd55d2c3c08cd3a1565
Grigor <ggrigoryan@bills.com>
2018-10-28 17:44:53 +0400

    Adding EUI configs

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -123,6 +123,7 @@ module.exports = {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
+      '@elastic/eui': '@elastic/eui/lib'
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
